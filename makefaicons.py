e6e713ddc5d4f78ad32396d7fa24596180ae1118
Ole Filippov <philya@gmail.com>
2014-12-22 17:38:02 +0200

    Correct translation

--- makefaicons.py
+++ makefaicons.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
+
+# Standard Library
 import xml.etree.ElementTree as ET
+
+# Vendor Libraries
 import yaml
+from jinja2 import Template
 
 def main():
     tree = ET.parse('res/fontawesome-webfont.svg')
@@ -22,15 +27,30 @@ def main():
     glyphs_out = open('build/glyphs.out', 'w')
 
     for key, idef in icond_map.items():
-        print key, idef['id']
         if key in glyph_map:
-            #print glyph_map[key].get('d')
+            # print glyph_map[key].get('horiz-adv-x')
             glyphs_out.write('<g id="%s"><path d="%s"/></g>\n' % (idef['id'], glyph_map[key].get('d')))
-
 
     glyphs_out.close()
 
+    glyphs = []
+    
+    for key, idef in icond_map.items():
+        if key in glyph_map:
+            glyph = idef
+            glyphel = glyph_map[key]
+            glyph['d'] = glyphel.get('d')
+            glyph['horiz-adv-x'] = glyphel.get('horiz-adv-x')
+            glyphs.append(glyph)
 
+    etemplatef = open('res/element-template.html', 'r')
+    etemplate = Template(etemplatef.read())
+    etemplatef.close()
+    econtent = etemplate.render(glyphs=glyphs)
+
+    efile = open('build/fa-icons.html', 'w')
+    efile.write(econtent)
+    efile.close()
 
 
 if __name__ == "__main__":
